b55eea46d03230713237a1d9a874221c80c93fd6
LeandroCeli <celileandrom@gmail.com>
2024-09-05 20:36:14 -0300

    Initial commit

---
+++ src/clases/Contacto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Celi Leandro
+ */
+public class Contacto {
+
+    private String apellido;
+    private String nombre;
+    private String dni;
+    private String direccion;
+    private String ciudad;
+
+    public Contacto(String apellido, String nombre, String dni, String direccion, String ciudad) {
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+}
---
+++ src/clases/Directorio.java
@@ -0,0 +1,35 @@
+package clases;
+
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Celi Leandro
+ */
+public class Directorio 
+{
+    
+    private TreeMap<Long, Contacto> directorio;
+
+    public Directorio() {
+    }
+   
+    public  void agregarContacto()
+    {
+        
+    }
+    public void buscarTelefono()
+    {
+        
+    }
+    
+    
+    public void buscarContacto()
+    {
+        
+    }
+    
+    public void borrarContacto(){
+        
+    }
+}
