d1ec01657e09c898a1263be5d02449566ec83fac
Luciano Colombero <lucianocolombero@gmail.com>
2024-09-08 18:02:47 -0300

    Metodos
    agregue codigo a los metodos

--- src/clases/Directorio.java
+++ src/clases/Directorio.java
@@ -1,6 +1,10 @@
 package clases;
 
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 /**
  *
@@ -12,24 +16,35 @@ public class Directorio
     private TreeMap<Long, Contacto> directorio;
 
     public Directorio() {
+        directorio = new TreeMap<>();
     }
    
-    public  void agregarContacto()
-    {
-        
+    public  void agregarContacto(Long telefono, Contacto contacto){
+        directorio.put(telefono, contacto);
     }
-    public void buscarTelefono()
-    {
-        
+   
+    public Set<Long> buscarTelefono(String apellido) {
+        Set<Long> telefonos = new TreeSet<>();
+        for (Map.Entry<Long, Contacto> entry : directorio.entrySet()) {
+            if (entry.getValue().getApellido().equals(apellido)) {
+                telefonos.add(entry.getKey());
+            }
+        }
+        return telefonos;
     }
     
     
-    public void buscarContacto()
-    {
-        
+public ArrayList<Contacto> buscarContactos(String ciudad) {
+        ArrayList<Contacto> contactos = new ArrayList<>();
+        for (Contacto contacto : directorio.values()) {
+            if (contacto.getCiudad().equals(ciudad)) {
+                contactos.add(contacto);
+            }
+        }
+        return contactos;
     }
     
-    public void borrarContacto(){
-        
+    public void borrarContacto(Long telefono) {
+        directorio.remove(telefono);
     }
 }
